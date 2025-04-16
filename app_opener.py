ccf6cc222b6269675e995232f06b4b9fe2a555a6
JeromeRReduta <jrreduta@yahoo.com>
2025-04-16 16:02:18 -0700

    Fixed bug, refactored data reading/converting, added OBS support

--- app_opener.py
+++ app_opener.py
@@ -2,6 +2,7 @@ from pywinauto.application import Application
 import csv
 from factories.csv_reader_to_app_data import CSV_Reader_To_App_Data
 from argparse import ArgumentParser
+import app_openers as util
 
 description = """An app that opens up a list of files in a CSV, alongside any URLS.
 
@@ -13,29 +14,19 @@ def get_csv_path():
     parser.add_argument("csv_path")
     return parser.parse_args().csv_path
 
-class AppOpener:
-        
-    def __init__(self, file):        
-        values = csv.reader(file) 
-        next(values, None) # skips headers
-        self.__data_set = {CSV_Reader_To_App_Data.convert(row) for row in values}
-            
-    def openAll(self):
-        for data in self.__data_set:
-            for arg in data.urls:
-                Application(backend="uia").start(r'{} "{}'.format(data.path, arg))
-
 def main():
     try:
-        csv_path = get_csv_path()
+        csv_path = util.get_csv_path()
         print("Reading file: {}...".format(csv_path))
-        with open(csv_path, mode='r') as file:
+        with open(csv_path, mode="r") as file:
             print("Opening apps...")
-            AppOpener(file).openAll()
-        print("Done!")
+            commands = util.convert_file_to_commands(file)
+            util.open_all_sites(commands)
+            print("Program finished")
         
     except Exception as e:
         print("Oh my god its on fire {}\n".format(e))
         
 if __name__ == "__main__":
     main()
+    
---
+++ app_openers.py
@@ -0,0 +1,53 @@
+from pywinauto.application import Application
+import csv
+from factories.csv_reader_to_app_data import CSV_Reader_To_App_Data
+from argparse import ArgumentParser
+from itertools import starmap
+import os
+import subprocess
+
+# TODO: Bug - doesn't open up files w/o any urls, e.g. Spotify or OBS
+
+
+description = """An app that opens up a list of files in a CSV, alongside any URLS.
+
+                 For example, this app can open several Chrome tabs at given links."""
+
+def get_csv_path():
+    parser = ArgumentParser(description=description)
+    parser.add_argument("csv_path")
+    return parser.parse_args().csv_path
+
+def convert_file_to_commands(csv_file):
+    values = csv.reader(csv_file) 
+    next(values, None) # skips headers
+    temp = [convert_row_to_commands(row) for row in values]
+    return temp
+
+def convert_row_to_commands(row):
+    commands = []
+    if row[0] is None or row[0].strip == "":
+        print("ERROR - NO PROGRAM NAME LISTED")
+        exit(-1)
+    name = row[0].strip()
+    for i in range(1, len(row)):
+        if row[i] is None or row[i].strip() == "":
+            continue
+        arg = row[i].strip()
+        commands.append(r'{} "{}"'.format(name, arg)) # Terminal treats ' and " differently - we need ' on the outside or terminal doesn't recognize second line should be treated as one term
+    return commands if commands else [name]
+
+def open_all_sites(commands):
+    for command in commands:
+        for line in command:
+            if line.find("obs64") != -1:
+                print("We found an obs bois" + line)
+                open_obs(line)
+            else:
+                Application(backend="uia").start(line)
+            
+def open_obs(line):
+    """obs needs specific support because you have to start it from its directory or it won't work"""
+    obs64_index = line.find("obs64")
+    dir, exe = line[0:obs64_index], line[obs64_index:]
+    os.system(r'start /d "{}" {}'.format(dir, exe))
