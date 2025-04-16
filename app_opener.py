2815ac5a7f9851e18ead1f37b6739f9f5619377a
JeromeRReduta <jrreduta@yahoo.com>
2025-04-16 16:58:28 -0700

    Added docstrings to modules & funcs

--- app_opener.py
+++ app_opener.py
@@ -1,3 +1,7 @@
+"""Given a path to an "App-Opener-CSV" (i.e. a CSV specially formatted for this program) containing executable paths and urls,\
+    opens all files w/ their urls
+"""
+
 import app_openers as util
 from argparse import ArgumentParser
 
@@ -5,12 +9,24 @@ description = """An app that opens up a list of files in a CSV, alongside any UR
 
                  For example, this app can open several Chrome tabs at given links."""
 
-def get_csv_path():
+def set_up_args():
+    """Sets up command-line arguments for the program
+
+    Returns:
+        arg_parser_namespace: Namespace from parser.parse_args(). Since we added
+                                   "csv_path" as an argument, namespace.csv_path exists
+    """ 
     parser = ArgumentParser(description=description)
     parser.add_argument("csv_path")
-    return parser.parse_args().csv_path
+    return parser.parse_args()
 
 def open_all_apps(csv_path):
+    """Opens all apps listed in the CSV Path
+
+    Args:
+        csv_path (str): path to csv
+    """
+    
     print("Reading file: {}...".format(csv_path))
     with open(csv_path, mode="r") as file:
         print("Opening apps...")
@@ -19,8 +35,8 @@ def open_all_apps(csv_path):
 
 def main():
     try:
-        csv_path = get_csv_path()
-        open_all_apps(csv_path)
+        args = set_up_args()
+        open_all_apps(args.csv_path)
         print("Program finished")
         
     except Exception as e:
--- app_openers.py
+++ app_openers.py
@@ -1,13 +1,44 @@
+"""Utility methods for app_opener
+
+    Methods: 
+        * convert_file_to_commands(csv_file)
+        * convert_row_to_commands(row)
+        * open_all_sites(commands)
+        * open_obs(line)
+"""
+
 from pywinauto.application import Application
 import csv
 import os
+    
+
 
 def convert_file_to_commands(csv_file):
+    """Converts all values in a csv file into command-line arguments
+
+    Args:
+        csv_file (str): path to csv
+
+    Returns:
+        list[list[str]]: a nested list of command-line arguments # TODO: make this a flat list
+    """
+    
     values = csv.reader(csv_file) 
     next(values, None) # skips headers
     return [convert_row_to_commands(row) for row in values]
 
 def convert_row_to_commands(row):
+    """Converts one row in the csv to a list of command-line arguments\
+    
+    WARNING: Not having an app name in the CSV is a FATAL ERROR. If this happens, the program will EXIT.
+
+    Args:
+        row (list[str]): one row from a csv.reader iterable
+
+    Returns:
+        list[str]: a list of command-line arguments, i.e. ["name url", "name url1," "name url2,"...]
+                   if no urls were given, simply returns the name as a command-line argument
+    """
     commands = []
     if row[0] is None or row[0].strip == "":
         print("ERROR - NO PROGRAM NAME LISTED")
@@ -21,6 +52,12 @@ def convert_row_to_commands(row):
     return commands if commands else [name]
 
 def open_all_sites(commands):
+    """Runs all given commands through the command line. In this context, it opens all sites
+       in the CSV alongside its urls
+
+    Args:
+        commands (list[str]): all command-line args
+    """
     for command in commands:
         for line in command:
             if line.find("obs64") != -1:
@@ -30,7 +67,13 @@ def open_all_sites(commands):
                 Application(backend="uia").start(line)
             
 def open_obs(line):
-    """obs needs specific support because you have to start it from its directory or it won't work"""
+    """opens obs
+        
+        obs needs specific support because you have to start it from its directory or it won't work
+
+    Args:
+        line (str): command-line arg
+    """
     obs64_index = line.find("obs64")
     dir, exe = line[0:obs64_index], line[obs64_index:]
     os.system(r'start /d "{}" {}'.format(dir, exe))
