f8ac73c4815fbdf9b49fdd834a489d86a4964b3d
JeromeRReduta <jrreduta@yahoo.com>
2025-04-21 04:52:08 -0700

    added text I forgot in app_openers

--- app_openers.py
+++ app_openers.py
@@ -16,7 +16,7 @@ import subprocess
 """ TODO:
 
 1. new docstrings
-2. new readme describing the change and telling ppl they can go to pywinauto branch if they want old functionality
+2. new readme describing the change and telling ppl they can go to pywinauto branch if they want old functionality and no more dependencies (beyond stdlib)
 3. clean up code
 4. push
 
